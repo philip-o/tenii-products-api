741d5acd723796d405ac58159ece19deece8684a
Philip Ogunleye <philip_o@hotmail.co.uk>
2018-12-31 14:29:23 +0400

    Updated response

--- src/main/scala/com/ogunleye/tenii/products/model/api/Transaction.scala
+++ src/main/scala/com/ogunleye/tenii/products/model/api/Transaction.scala
@@ -5,7 +5,7 @@ case class Transaction(transactionId: String, accountId: String, teniiId: String
 case class ProcessTransactionResponse(transactionId: String, error: Option[String] = None)
 
 
-case class GetTransactionResponse(transactionId: List[String], teniiId: String, date: Option[String])
+case class GetTransactionResponse(transactionIds: List[String], teniiId: String, date: Option[String])
 
 case class GetTransactionErrorResponse(error: String)
 
